edb7cd1a09d52c02308f191a43e076aac47f0995
sweety kaushik <sweetykaushik33@gmail.com>
2020-12-19 23:19:48 +0000

    library

---
+++ library/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2020 Online Library Management System |<a href="" target="_blank"  > Designed by : Sweetykaushik</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
